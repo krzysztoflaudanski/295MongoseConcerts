f8c98909449b0c5ed3d177717abc7b0d140a347f
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-11-05 13:54:30 +0100

    init

---
+++ routes/seats.routes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const db = require('../db');
+const { generateRandomId } = require('./../utils/helpers');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.json(db.seats);
+});
+
+// Dla GET /seats/:id
+router.get('/:id', (req, res) => {
+    const seatId = parseInt(req.params.id);
+    const seat = db.seats.find(seat => seat.id === seatId);
+    if (seat) {
+        res.json(seat);
+    } else {
+        res.status(404).json({ error: 'Seat not found' });
+    }
+});
+
+router.post('/', (req, res) => {
+    const { day, seat, client, email } = req.body;
+
+    if (day && seat && client && email) {
+        // Sprawdź, czy miejsce w dniu jest już zajęte
+        const isSeatTaken = db.seats.some(existingSeat => existingSeat.day === day && existingSeat.seat === seat);
+
+        if (isSeatTaken) {
+            res.status(409).json({ error: 'Seat is already taken' });
+        } else {
+            const newId = generateRandomId();
+            const newSeat = { id: newId, day, seat, client, email };
+            db.seats.push(newSeat);
+            res.status(201).json({ message: 'OK' });
+            req.io.emit('seatsUpdate', db.seats)
+        }
+    } else {
+        res.status(400).json({ error: 'Invalid data format' });
+    }
+});
+
+// Dla DELETE /seats/:id
+router.delete('/:id', (req, res) => {
+    const seatId = parseInt(req.params.id);
+    const seatIndex = db.seats.findIndex(seat => seat.id === seatId);
+
+    if (seatIndex !== -1) {
+        db.seats.splice(seatIndex, 1);
+        res.json({ message: 'OK' });
+    } else {
+        res.status(404).json({ error: 'Seat not found' });
+    }
+});
+
+// Dla PUT /seats/:id
+router.put('/:id', (req, res) => {
+    const seatId = parseInt(req.params.id);
+    const { day, seat, client, email } = req.body;
+
+    if (day && seat && client && email) {
+        const seatIndex = db.seats.findIndex(seat => seat.id === seatId);
+
+        if (seatIndex !== -1) {
+            db.seats[seatIndex] = { id: seatId, day, seat, client, email };
+            res.json({ message: 'OK' });
+        } else {
+            res.status(404).json({ error: 'Seat not found' });
+        }
+    } else {
+        res.status(400).json({ error: 'Invalid data format' });
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
+const socketIo = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const testimonialsRouter = require('./routes/testimonials.routes'); // Importuj router
+const concertsRouter = require('./routes/concerts.routes');
+const seatsRouter = require('./routes/seats.routes');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use('/api/testimonials', testimonialsRouter); // add post routes to server
+app.use('/api/concerts', concertsRouter);
+app.use('/api/seats', seatsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Not found...' });
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('Klient podłączony');
+
+});
